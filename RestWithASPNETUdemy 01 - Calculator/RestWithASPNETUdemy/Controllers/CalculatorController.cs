ccc807d5290cfbaf3a4974878cd9e48d1f0d7f0c
luciano-espindula <luc.aespindula@gmail.com>
2019-11-17 18:05:07 -0300

    Implementing REST Calculator

--- RestWithASPNETUdemy 01 - Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
+++ RestWithASPNETUdemy 01 - Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -8,12 +8,65 @@ namespace RestWithASPNETUdemy.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class CalculatorController : ControllerBase {
-        // GET api/values/5/5
-        [HttpGet("{firstNumber}/{secondNumber}")]
+        // GET api/Calculator/sum/5/5
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
         public IActionResult Sum(string firstNumber, string secondNumber) {
             if (IsNumeric(firstNumber) && IsNumeric(secondNumber)) {
                 var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
                 return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid imput");
+        }
+
+        // GET api/Calculator/subtraction/5/5
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtraction(string firstNumber, string secondNumber) {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber)) {
+                var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid imput");
+        }
+
+        // GET api/Calculator/multiplication/5/5
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber) {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber)) {
+                var multiplication = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(multiplication.ToString());
+            }
+            return BadRequest("Invalid imput");
+        }
+
+        // GET api/Calculator/division/5/5
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public IActionResult Division(string firstNumber, string secondNumber) {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber)) {
+                if (ConvertToDecimal(secondNumber) > 0) {
+                    var division = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                    return Ok(division.ToString());
+                }
+                return BadRequest("Division by Zero");
+            }
+            return BadRequest("Invalid imput");
+        }
+
+        // GET api/Calculator/mean/5/5
+        [HttpGet("mean/{firstNumber}/{secondNumber}")]
+        public IActionResult Mean(string firstNumber, string secondNumber) {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber)) {
+                var mean = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(mean.ToString());
+            }
+            return BadRequest("Invalid imput");
+        }
+
+        // GET api/Calculator/square-root/5
+        [HttpGet("square-root/{Number}")]
+        public IActionResult SquareRoot(string Number) {
+            if (IsNumeric(Number)) {
+                var squareRoot = Math.Sqrt((double)ConvertToDecimal(Number));
+                return Ok(squareRoot.ToString());
             }
             return BadRequest("Invalid imput");
         }
