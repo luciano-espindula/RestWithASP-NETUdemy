13ae9043e8fc96d7f117ca0e28feb034da8c578b
luciano-espindula <luc.aespindula@gmail.com>
2020-04-07 20:56:31 -0300

    Content Negotiation

---
+++ RestWithASPNETUdemy 10 - Content Negotiation/RestWithASPNETUdemy/Model/Base/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace RestWithASPNETUdemy.Model.Base
+{
+    //LAE - precisaria definir para todos os atributos
+    //[DataContract]
+    public class BaseEntity
+    {
+        public long? Id { get; set; }
+    }
+}
---
+++ RestWithASPNETUdemy 10 - Content Negotiation/RestWithASPNETUdemy/Model/Person.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNETUdemy.Model.Base;
+
+namespace RestWithASPNETUdemy.Model
+{
+    public class Person : BaseEntity
+    {
+        //public long? Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+    }
+}
