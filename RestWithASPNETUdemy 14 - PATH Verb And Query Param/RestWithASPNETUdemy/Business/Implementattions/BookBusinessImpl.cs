a22d8d8836bad07bdfcbc02f543d86631197339e
luciano-espindula <luc.aespindula@gmail.com>
2020-04-10 20:17:09 -0300

    PATH Verb And Query Param

---
+++ RestWithASPNETUdemy 14 - PATH Verb And Query Param/RestWithASPNETUdemy/Business/Implementattions/BookBusinessImpl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using RestWithASPNETUdemy.Data.Converters;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Repository.Generic;
+
+namespace RestWithASPNETUdemy.Business.Implementattions
+{
+    public class BookBusinessImpl : IBookBusiness
+    {
+        private IRepository<Book> _repository;
+
+        private readonly BookConverter _converter;
+
+        public BookBusinessImpl(IRepository<Book> repository)
+        {
+            _repository = repository;
+            _converter = new BookConverter();
+        }
+
+        // Metodo responsável por criar uma nova pessoa
+        // nesse momento adicionamos o objeto ao contexto
+        // e finalmente salvamos as mudanças no contexto
+        // na base de dados
+        public BookVO Create(BookVO book)
+        {
+            var bookEntity = _converter.Parse(book);
+            bookEntity = _repository.Create(bookEntity);
+            return _converter.Parse(bookEntity);
+        }
+
+        // Método responsável por retornar uma pessoa
+        public BookVO FindById(long id)
+        {
+            return _converter.Parse(_repository.FindById(id));
+        }
+
+        // Método responsável por retornar todas as pessoas
+        public List<BookVO> FindAll()
+        {
+            return _converter.ParseList(_repository.FindAll());
+        }
+
+        // Método responsável por atualizar uma pessoa
+        public BookVO Update(BookVO book)
+        {
+            var bookEntity = _converter.Parse(book);
+            bookEntity = _repository.Update(bookEntity);
+            return _converter.Parse(bookEntity);
+        }
+
+        // Método responsável por deletar
+        // uma pessoa a partir de um ID
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+
+    
+}
---
+++ RestWithASPNETUdemy 14 - PATH Verb And Query Param/RestWithASPNETUdemy/Business/Implementattions/PersonBusinessImpl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using RestWithASPNETUdemy.Data.Converters;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Repository.Generic;
+
+namespace RestWithASPNETUdemy.Business.Implementattions
+{
+    public class PersonBusinessImpl : IPersonBusiness
+    {
+
+        private IRepository<Person> _repository;
+
+        private readonly PersonConverter _converter;
+
+        public PersonBusinessImpl(IRepository<Person> repository)
+        {
+            _repository = repository;
+            _converter = new PersonConverter();
+        }
+
+        // Metodo responsável por criar uma nova pessoa
+        // nesse momento adicionamos o objeto ao contexto
+        // e finalmente salvamos as mudanças no contexto
+        // na base de dados
+        public PersonVO Create(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person);
+            personEntity = _repository.Create(personEntity);
+            return _converter.Parse(personEntity);
+        }
+
+        // Método responsável por retornar uma pessoa
+        public PersonVO FindById(long id)
+        {
+            return _converter.Parse(_repository.FindById(id));
+        }
+
+        // Método responsável por retornar todas as pessoas
+        public List<PersonVO> FindAll()
+        {
+            return _converter.ParseList(_repository.FindAll());
+        }
+
+        // Método responsável por atualizar uma pessoa
+        public PersonVO Update(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person);
+            personEntity = _repository.Update(personEntity);
+            return _converter.Parse(personEntity);
+        }
+
+        // Método responsável por deletar
+        // uma pessoa a partir de um ID
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
