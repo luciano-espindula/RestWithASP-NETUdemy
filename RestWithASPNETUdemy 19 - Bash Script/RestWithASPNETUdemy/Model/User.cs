6494aef48e4161f54673951d71f520a63deaf061
luciano-espindula <luc.aespindula@gmail.com>
2020-04-22 16:20:48 -0300

    Bash Script

---
+++ RestWithASPNETUdemy 19 - Bash Script/RestWithASPNETUdemy/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNETUdemy.Model
+{
+    [Table("users")]
+    public class User
+    {
+        [Column("id")]
+        public long? Id { get; set; }
+
+        [Column("Login")]
+        public string Login { get; set; }
+
+        [Column("AccessKey")]
+        public string AccessKey { get; set; }
+
+    }
+}
---
+++ RestWithASPNETUdemy 19 - Bash Script/RestWithASPNETUdemy/Repository/Implementattions/UserRepositoryImpl.cs
@@ -0,0 +1,25 @@
+﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using System.Linq;
+
+namespace RestWithASPNETUdemy.Repository.Implementattions
+{
+    public class UserRepositoryImpl : IUserRepository
+    {
+
+        private MySQLContext _context;
+
+        public UserRepositoryImpl(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        // Método responsável por retornar uma pessoa
+        public User FindByUser(string login)
+        {
+            return _context.Users.SingleOrDefault(u => u.Login.Equals(login));
+        }
+
+       
+    }
+}
